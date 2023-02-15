a0a0cb0bf5f7569b99eac76131285e90db6fb5e1
Adam Celarek <git_commits@xibo.at>
2023-02-15 15:20:52 +0100

    fix licenses

--- nucleus/camera/stored_positions.h
+++ nucleus/camera/stored_positions.h
@@ -1,5 +1,5 @@
 /*****************************************************************************
- * Alpine Renderer
+ * Alpine Terrain Renderer
  * Copyright (C) 2022 Adam Celarek
  *
  * This program is free software: you can redistribute it and/or modify
