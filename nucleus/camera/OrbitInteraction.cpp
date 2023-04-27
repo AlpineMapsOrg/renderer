505ab8fbe80250edaca67464adacb4c02df2040f
Adam Celarek <5292991+adam-ce@users.noreply.github.com>
2023-04-27 12:28:09 +0200

    implement better (distance based) touch interaction

--- nucleus/camera/OrbitInteraction.cpp
+++ nucleus/camera/OrbitInteraction.cpp
@@ -19,6 +19,7 @@
 
 #include "OrbitInteraction.h"
 #include "AbstractDepthTester.h"
+#include "glm/ext/scalar_constants.hpp"
 
 #include <QDebug>
 
@@ -26,35 +27,14 @@ namespace nucleus::camera {
 
 std::optional<Definition> OrbitInteraction::mouse_press_event(const event_parameter::Mouse& e, Definition camera, AbstractDepthTester* depth_tester)
 {
-    m_operation_centre = depth_tester->position(camera.to_ndc({ e.point.pressPosition().x(), e.point.pressPosition().y() }));
-
-    auto degFromUp = glm::degrees(glm::acos(glm::dot(camera.z_axis(), glm::dvec3(0, 0, 1))));
-    if (m_operation_centre.z > camera.position().z || degFromUp > 80.0f) {
-        m_move_vertical = true;
-    } else {
-        m_move_vertical = false;
-    }
+    start(e.point.position(), camera, depth_tester);
     return {};
 }
 
 std::optional<Definition> OrbitInteraction::mouse_move_event(const event_parameter::Mouse& e, Definition camera, AbstractDepthTester* depth_tester)
 {
     if (e.buttons == Qt::LeftButton && !m_key_ctrl && !m_key_alt) {
-        m_operation_centre_screen = glm::vec2(e.point.position().x(), e.point.position().y());
-        glm::dvec3 camRay = camera.ray_direction(camera.to_ndc({ e.point.position().x(), e.point.position().y() }));
-        double denom = glm::dot(glm::dvec3(0, 0, 1.0), camRay);
-        double distance = 0;
-        if (denom != 0) {
-            distance = glm::dot((m_operation_centre - camera.position()), glm::dvec3(0, 0, 1.0)) / denom;
-        }
-        auto mouseInWorld = camera.position() + (camRay * distance);
-
-        if (m_move_vertical) {
-            const auto delta = e.point.position() - e.point.lastPosition();
-            camera.move(camera.x_axis() * -delta.x() + camera.y_axis() * delta.y());
-        } else {
-            camera.move(glm::dvec3(m_operation_centre.x - mouseInWorld.x, m_operation_centre.y - mouseInWorld.y, 0));
-        }
+        pan(e.point.position(), e.point.lastPosition(), &camera, depth_tester);
     }
     if (e.buttons == Qt::MiddleButton || (e.buttons == Qt::LeftButton && m_key_ctrl && !m_key_alt)) {
         m_operation_centre_screen = glm::vec2(e.point.pressPosition().x(), e.point.pressPosition().y());
@@ -64,11 +44,7 @@ std::optional<Definition> OrbitInteraction::mouse_move_event(const event_paramet
     if (e.buttons == Qt::RightButton || (e.buttons == Qt::LeftButton && !m_key_ctrl && m_key_alt)) {
         m_operation_centre_screen = glm::vec2(e.point.pressPosition().x(), e.point.pressPosition().y());
         const auto delta = e.point.position() - e.point.lastPosition();
-
-        float distance = glm::distance(m_operation_centre, camera.position());
-        float dist = -1.0 * std::max((distance / 1200), 0.07f);
-
-        camera.move(glm::normalize(m_operation_centre - camera.position()) * ((delta.y() - delta.x()) * (double)dist));
+        zoom((delta.y() - delta.x()) / 2.f, &camera, depth_tester);
     }
 
     if (e.buttons == Qt::NoButton)
@@ -79,64 +55,58 @@ std::optional<Definition> OrbitInteraction::mouse_move_event(const event_paramet
 
 std::optional<Definition> OrbitInteraction::touch_event(const event_parameter::Touch& e, Definition camera, AbstractDepthTester* depth_tester)
 {
-    glm::ivec2 first_touch = { e.points[0].position().x(), e.points[0].position().y() };
-    glm::ivec2 second_touch;
-    if (e.points.size() >= 2)
-        second_touch = { e.points[1].position().x(), e.points[1].position().y() };
-
-    // ugly code, but it prevents the following:
-    // touch 1 at pos a, touch 2 at pos b
-    // release touch 1, touch 2 becomes new touch 1. movement from b to a is initiated.
-    if (m_was_double_touch) {
-        m_previous_first_touch = first_touch;
-    }
-    m_was_double_touch = false;
-
-    if (e.is_end_event) {
-        m_was_double_touch = e.points.size() >= 2;
-        return {};
-    }
-    if (e.is_begin_event) {
-        m_previous_first_touch = first_touch;
-        m_previous_second_touch = second_touch;
-        return {};
-    }
     // touch move
+    if (e.points.size() == 1 && e.points[0].state() == QEventPoint::State::Pressed) {
+        start(e.points[0].position(), camera, depth_tester);
+        return {};
+    }
+    if (e.points.size() == 2 && e.points[1].state() == QEventPoint::State::Released) {
+        start(e.points[0].position(), camera, depth_tester);
+        return {};
+    }
+    if (e.points.size() == 2 && e.points[0].state() == QEventPoint::State::Released) {
+        start(e.points[1].position(), camera, depth_tester);
+        return {};
+    }
     if (e.points.size() == 1) {
-        const auto delta = first_touch - m_previous_first_touch;
-        camera.pan(glm::vec2(delta) * 10.0f);
+        pan(e.points[0].position(), e.points[0].lastPosition(), &camera, depth_tester);
+        return camera;
     }
     if (e.points.size() == 2) {
-        const auto previous_centre = (m_previous_first_touch + m_previous_second_touch) / 2;
-        const auto current_centre = (first_touch + second_touch) / 2;
-        const auto pitch = -(current_centre - previous_centre).y;
+        const auto current_centre = (e.points[0].position() + e.points[1].position()) * 0.5f;
+        if (e.points[0].state() == QEventPoint::State::Pressed || e.points[1].state() == QEventPoint::State::Pressed) {
+            m_operation_centre_screen = { current_centre.x(), current_centre.y() };
+            start(current_centre, camera, depth_tester);
+            return {};
+        }
+        const auto previous_centre = (e.points[0].lastPosition() + e.points[1].lastPosition()) * 0.5f;
+        const auto first_touch = glm::vec2(e.points[0].position().x(), e.points[0].position().y());
+        const auto second_touch = glm::vec2(e.points[1].position().x(), e.points[1].position().y());
+        const auto previous_first_touch = glm::vec2(e.points[0].lastPosition().x(), e.points[0].lastPosition().y());
+        const auto previous_second_touch = glm::vec2(e.points[1].lastPosition().x(), e.points[1].lastPosition().y());
+        const auto pitch = -(current_centre.y() - previous_centre.y());
 
-        const auto previous_yaw_dir = glm::normalize(glm::vec2(m_previous_first_touch - m_previous_second_touch));
+        const auto previous_yaw_dir = glm::normalize(glm::vec2(previous_first_touch - previous_second_touch));
         const auto previous_yaw_angle = std::atan2(previous_yaw_dir.y, previous_yaw_dir.x);
         const auto current_yaw_dir = glm::normalize(glm::vec2(first_touch - second_touch));
         const auto current_yaw_angle = std::atan2(current_yaw_dir.y, current_yaw_dir.x);
-//        qDebug() << "prev: " << previous_yaw_angle << ", curr: " << current_yaw_angle;
 
-        const auto yaw = (current_yaw_angle - previous_yaw_angle) * 600;
-        camera.orbit(glm::vec2(yaw, pitch) * 0.1f);
+        const auto yaw = (current_yaw_angle - previous_yaw_angle) * 360 / glm::pi<double>();
+        camera.orbit_clamped(m_operation_centre, glm::vec2(yaw, pitch));
 
-        const auto previous_dist = glm::length(glm::vec2(m_previous_first_touch - m_previous_second_touch));
+        const auto previous_dist = glm::length(glm::vec2(previous_first_touch - previous_second_touch));
         const auto current_dist = glm::length(glm::vec2(first_touch - second_touch));
-        camera.zoom((previous_dist - current_dist) * 10);
+        zoom(-(previous_dist - current_dist), &camera, depth_tester);
+        return camera;
     }
-    m_previous_first_touch = first_touch;
-    m_previous_second_touch = second_touch;
-    return camera;
+    return {};
 }
 
 std::optional<Definition> OrbitInteraction::wheel_event(const event_parameter::Wheel& e, Definition camera, AbstractDepthTester* depth_tester)
 {
     m_operation_centre_screen = glm::vec2(e.point.position().x(), e.point.position().y());
-    glm::dvec3 hit = depth_tester->position(camera.to_ndc({ e.point.position().x(), e.point.position().y() }));
-    float distance = glm::distance(hit, camera.position());
-
-    float dist = std::max((distance / 1500), 0.07f);
-    camera.move(glm::normalize(hit - camera.position()) * (e.angle_delta.y() * (double)dist));
+    start(e.point.position(), camera, depth_tester);
+    zoom(e.angle_delta.y() / 15.f, &camera, depth_tester);
     return camera;
 }
 
@@ -164,5 +134,43 @@ std::optional<Definition> OrbitInteraction::key_release_event(const QKeyCombinat
 
 std::optional<glm::vec2> OrbitInteraction::get_operation_centre(){
     return m_operation_centre_screen;
+}
+
+void OrbitInteraction::start(const QPointF& position, const Definition& camera, AbstractDepthTester* depth_tester)
+{
+    m_operation_centre = depth_tester->position(camera.to_ndc({ position.x(), position.y() }));
+
+    auto degFromUp = glm::degrees(glm::acos(glm::dot(camera.z_axis(), glm::dvec3(0, 0, 1))));
+    if (m_operation_centre.z > camera.position().z || degFromUp > 80.0f) {
+        m_move_vertical = true;
+    } else {
+        m_move_vertical = false;
+    }
+}
+
+void OrbitInteraction::pan(const QPointF& position, const QPointF& last_position, Definition* camera, AbstractDepthTester* depth_tester)
+{
+    m_operation_centre_screen = glm::vec2(position.x(), position.y());
+    glm::dvec3 camRay = camera->ray_direction(camera->to_ndc({ position.x(), position.y() }));
+    double denom = glm::dot(glm::dvec3(0, 0, 1.0), camRay);
+    double distance = 0;
+    if (denom != 0) {
+        distance = glm::dot((m_operation_centre - camera->position()), glm::dvec3(0, 0, 1.0)) / denom;
+    }
+    auto mouseInWorld = camera->position() + (camRay * distance);
+
+    if (m_move_vertical) {
+        const auto delta = position - last_position;
+        camera->move(camera->x_axis() * -delta.x() + camera->y_axis() * delta.y());
+    } else {
+        camera->move(glm::dvec3(m_operation_centre.x - mouseInWorld.x, m_operation_centre.y - mouseInWorld.y, 0));
+    }
+}
+
+void OrbitInteraction::zoom(float amount, Definition* camera, AbstractDepthTester* depth_tester)
+{
+    float distance = glm::distance(m_operation_centre, camera->position());
+    distance = std::max(distance / 100, 0.07f);
+    camera->move(glm::normalize(m_operation_centre - camera->position()) * double(amount * distance));
 }
 }
