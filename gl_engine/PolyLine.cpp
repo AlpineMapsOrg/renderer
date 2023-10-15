9b01ca3828f3174d24ac65dc1ac00a101141f063
Jakob <jakob.g.maier@gmail.com>
2023-10-15 19:37:30 +0200

    Add PolyLine

---
+++ gl_engine/PolyLine.cpp
@@ -0,0 +1,47 @@
+/*****************************************************************************
+ * Alpine Terrain Builder
+ * Copyright (C) 2022 alpinemaps.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+
+#include "PolyLine.h"
+#include "ShaderProgram.h"
+
+namespace gl_engine {
+
+static const char* const debugVertexShaderSource = R"(
+  layout(location = 0) in vec4 a_position;
+  uniform highp mat4 matrix;
+  void main() {
+    gl_Position = matrix * a_position;
+  })";
+
+static const char* const debugFragmentShaderSource = R"(
+  out lowp vec4 out_Color;
+  void main() {
+     out_Color = vec4(1.0, 0.0, 0.0, 1.0);
+  })";
+
+PolyLine::PolyLine(const std::vector<glm::vec3>& points) 
+    : m_shader(std::make_unique<ShaderProgram>(debugVertexShaderSource, debugFragmentShaderSource))
+{
+    // TODO
+}
+
+void PolyLine::draw() const {
+    // TODO
+}
+
+}
---
+++ gl_engine/PolyLine.h
@@ -0,0 +1,37 @@
+/*****************************************************************************
+ * Alpine Terrain Builder
+ * Copyright (C) 2022 alpinemaps.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+
+#pragma once
+
+#include <memory>
+#include <vector>
+#include <glm/glm.hpp>
+
+namespace gl_engine {
+
+class ShaderProgram;
+
+class PolyLine {
+public:
+    PolyLine(const std::vector<glm::vec3>& points);
+    void draw() const;
+private:
+    std::unique_ptr<ShaderProgram> m_shader;
+};
+
+}
