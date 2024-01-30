3747d640cfb80c270d78c90369958f2b2c490aef
Adam Celarek <5292991+adam-ce@users.noreply.github.com>
2024-01-30 19:37:48 +0100

    fix alignment on input for mobile webassembly.

--- nucleus/utils/texture_compression.cpp
+++ nucleus/utils/texture_compression.cpp
@@ -59,20 +59,23 @@ std::vector<uint8_t> nucleus::utils::texture_compression::to_etc1(const QImage& 
     }
     assert(qimage.width() == qimage.height());
     assert(qimage.width() % 16 == 0);
+    assert(qimage.bytesPerLine() * qimage.height() == qimage.width() * qimage.height() * 4);
 
-    // struct alignas(64) AlignedBlock
-    // {
-    //     std::array<uint8_t, 64> data;
-    // };
-    // static_assert(sizeof(AlignedBlock) == 64);
+    struct alignas(16) AlignedBlock
+    {
+        std::array<uint8_t, 16> data;
+    };
+    static_assert(sizeof(AlignedBlock) == 16);
 
-    const auto n_bytes = qimage.width() * qimage.height() / 2;
-    // assert(n_bytes % sizeof(AlignedBlock) == 0);
+    const auto n_bytes_in = qimage.width() * qimage.height() * 4;
+    const auto n_bytes_out = qimage.width() * qimage.height() / 2;
+    assert(n_bytes_in % sizeof(AlignedBlock) == 0);
 
-    // auto buffer = std::vector<AlignedBlock>(n_bytes / sizeof(AlignedBlock));
+    auto aligned_in = std::vector<AlignedBlock>(n_bytes_in / sizeof(AlignedBlock));
+    std::copy(qimage.bits(), qimage.bits() + n_bytes_in, aligned_in.data()->data.data());
     // auto data_ptr = reinterpret_cast<uchar*>(buffer.data());
 
-    std::vector<uint8_t> compressed(n_bytes);
+    std::vector<uint8_t> compressed(n_bytes_out);
     const auto result = goofy::compressETC1(compressed.data(), qimage.bits(), qimage.width(), qimage.height(), qimage.width() * 4);
     assert(result == 0);
 
