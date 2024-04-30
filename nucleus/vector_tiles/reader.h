827ca0dcf093d774e1b4999c946e884733123518
joergchristian <e12213117@student.tuwien.ac.at>
2024-04-30 16:22:34 +0200

    format file and remove obsolete code

--- nucleus/vector_tiles/reader.h
+++ nucleus/vector_tiles/reader.h
@@ -16,26 +16,20 @@ struct EawsRegion
     std::string start_date="";                              // The day the regions was defined
     std::string end_date="";                                // Only for outdated regions: The day the regions expired
     std::vector<glm::ivec2> verticesInLocalCoordinates; // the vertices of the region's bounding polygon
-    std::vector<glm::vec2> verticesInGlobalCoordinates; // the vertices of the region's bounding polygon
 };
 
-namespace vector_tile
-{
-    namespace reader
-    {
-        /* Reads all EAWS regions stored in a provided vector tile
-         * The mvt file is expected to contain three layers: "micro-regions", "micro-regions_elevation", "outline". The first one is relevant for this class.
-         * The layer "micro-regions" contains several features, each feature representing one micro region.
-         * Every feature contains at least one poperty and one geometry, the latter one holding the vertices of the region's boundary polygon.
-         * Every Property has an id. There must be at least one poperty with its id holding the name-string of the region (e.g. "FR-64").
-         * Further properties can exist with ids (all string) "alt-id", "start_date", "end_date".
-         * If a region has the property with id "end_date" this region is outdated and was substituted by other regions.
-         * This old regions is kept in the collection to allow processing of older bulletins that were using this region
-         * @param inputFilePath : Path to a mvt file
-         * @param nameOfLayerWithEawsRegions : name of the layer that contains the EAWS micro regions and the vertices of their boundary polygon
-         */
-        tl::expected<std::vector<EawsRegion>, std::string> eaws_regions(
-            const QByteArray& inputData, const std::string& nameOfLayerWithEawsRegions = "micro-regions");
-    }
-}
+namespace vector_tile::reader {
+/* Reads all EAWS regions stored in a provided vector tile
+ * The mvt file is expected to contain three layers: "micro-regions", "micro-regions_elevation", "outline". The first one is relevant for this class.
+ * The layer "micro-regions" contains several features, each feature representing one micro region.
+ * Every feature contains at least one poperty and one geometry, the latter one holding the vertices of the region's boundary polygon.
+ * Every Property has an id. There must be at least one poperty with its id holding the name-string of the region (e.g. "FR-64").
+ * Further properties can exist with ids (all string) "alt-id", "start_date", "end_date".
+ * If a region has the property with id "end_date" this region is outdated and was substituted by other regions.
+ * This old regions is kept in the collection to allow processing of older bulletins that were using this region
+ * @param inputFilePath : Path to a mvt file
+ * @param nameOfLayerWithEawsRegions : name of the layer that contains the EAWS micro regions and the vertices of their boundary polygon
+ */
+tl::expected<std::vector<EawsRegion>, std::string> eaws_regions(const QByteArray& inputData, const std::string& nameOfLayerWithEawsRegions = "micro-regions");
+} // namespace vector_tile::reader
 #endif // READER_H
