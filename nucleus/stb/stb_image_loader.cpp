d6828d538c140a9b127c24caa8f28f9183797348
Adam Celarek <5292991+adam-ce@users.noreply.github.com>
2024-07-26 08:01:15 +0200

    remove extra compilation unit for stb_image, this is dangerous because the header might give different definitions if the preprocessor stuff is not defined.

--- nucleus/stb/stb_image_loader.cpp
+++ nucleus/stb/stb_image_loader.cpp
@@ -1,6 +1,7 @@
 /*****************************************************************************
- * Alpine Maps and weBIGeo
+ * AlpineMaps.org
  * Copyright (C) 2024 Gerald Kimmersdorfer
+ * Copyright (C) 2024 Adam Celarek
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,7 +18,24 @@
  *****************************************************************************/
 
 #include "stb_image_loader.h"
+
+// Limit the dimensions of images to 8192x8192. This is already quite restricting
+// in terms of that a lot of GPUs don't support textures that large. Make sure
+// you know what you are doing, before you change this value.
+#define STBI_MAX_DIMENSIONS 8192
+
+// Only include the code for the formats we need. This reduces the code footprint
+// and allows for faster compilation times. Add more formats here, if you need them.
+// For possible options check the stb_image.h file.
+#define STBI_ONLY_JPEG
+#define STBI_ONLY_PNG
+
+// Remove if you intend to use stbi_failure_reason()
+#define STBI_NO_FAILURE_STRINGS
+
+#define STB_IMAGE_IMPLEMENTATION
 #include <stb_slim/stb_image.h>
+
 #include <stdexcept>
 #include <QFile>
 
