570acd94c86582b8a47b57d92ca74f1b3b48216f
Adam Celarek <5292991+adam-ce@users.noreply.github.com>
2024-08-11 20:53:41 +0200

    support for gadgets

--- app/ModelBinding.cpp
+++ app/ModelBinding.cpp
@@ -29,39 +29,86 @@ void ModelBinding::classBegin() { }
 
 void ModelBinding::componentComplete()
 {
+    assert(m_complete == false);
     if (!m_qml_target.isValid()) {
-        qWarning() << "m_qml_target invalid!";
+        qWarning() << "ModelBinding: QML property invalid!";
+        if (m_model)
+            qWarning() << "ModelBinding: model target: " << m_model;
+        if (m_property.size() > 0)
+            qWarning() << "ModelBinding: model property: " << m_property;
+        return;
+    }
+    if (!m_model) {
+        qWarning() << "ModelBinding: model target invalid!";
+        if (m_qml_target.isValid())
+            qWarning() << "ModelBinding: QML target: " << m_qml_target.object() << ", property: " << m_qml_target.property().name();
+        if (m_property.size() > 0)
+            qWarning() << "ModelBinding: model property: " << m_property;
         return;
     }
 
     const auto* this_meta = this->metaObject();
-
-    const auto write_model_slot = this_meta->indexOfSlot("write_model()");
-    connect(m_qml_target.object(), m_qml_target.property().notifySignal(), this, this_meta->method(write_model_slot));
-
-    const auto* model_meta = m_model->metaObject();
-    const auto model_property_index = model_meta->indexOfProperty(m_property.toStdString().c_str());
-    if (model_property_index < 0) {
-        qWarning() << "Property " << m_property << " of object " << m_model << " not found!";
-        return;
-    }
-    m_model_meta_property = m_model->metaObject()->property(model_property_index);
-    if (m_receive_model_updates) {
-        if (!m_model_meta_property.hasNotifySignal()) {
-            qWarning() << "Property " << m_property << " of object " << m_model << " has no notification signal!";
-            return;
+    {
+        const auto property_chain = m_property.split('.');
+        if (property_chain.size() > 2) {
+            qWarning() << "ModelBinding: Property " << m_property << " of object " << m_model
+                       << " not supported! At most 2 levels are supported (gadget.property).";
         }
 
-        const auto qml_write_slot = this_meta->indexOfSlot("write_qml()");
-        connect(m_model, m_model_meta_property.notifySignal(), this, this_meta->method(qml_write_slot));
+        const auto* model_meta = m_model->metaObject();
+        if (property_chain.size() == 1) {
+            const auto property_index = model_meta->indexOfProperty(m_property.toStdString().c_str());
+            if (property_index < 0) {
+                qWarning() << "ModelBinding:" << m_property << "not found in object" << m_model << "when binding" << m_qml_target.object()
+                           << ", property: " << m_qml_target.property().name() << "!";
+                return;
+            }
+            m_meta_property = model_meta->property(property_index);
+        } else {
+            const auto gadget_index = model_meta->indexOfProperty(property_chain.at(0).toStdString().c_str());
+            if (gadget_index < 0) {
+                qWarning() << "ModelBinding:" << property_chain.at(0) << "not found in object" << m_model << "when binding" << m_qml_target.object()
+                           << ", property: " << m_qml_target.property().name() << "!";
+                return;
+            }
+            auto gadget_meta_property = model_meta->property(gadget_index);
+            if (!(gadget_meta_property.metaType().flags() & QMetaType::TypeFlag::IsGadget)) {
+                qWarning() << "ModelBinding: Property " << m_property << " of object " << m_model
+                           << " not supported! If two levels are used, the first must be a gadget (gadget.property)."
+                           << "If you have an object hierarchy, select the correct object in the target.";
+                return;
+            }
+            auto gadget_meta_object = gadget_meta_property.metaType().metaObject();
+            const auto property_index = gadget_meta_object->indexOfProperty(property_chain.at(1).toStdString().c_str());
+            if (property_index < 0) {
+                qWarning() << "ModelBinding:" << property_chain.at(1) << "not found in gadget " << gadget_meta_object << "of" << m_model << "when binding"
+                           << m_qml_target.object() << ", property: " << m_qml_target.property().name() << "!";
+                return;
+            }
+            m_gadget_meta_property = gadget_meta_property;
+            m_meta_property = gadget_meta_object->property(property_index);
+        }
     }
 
+    m_complete = true;
     if (m_default_value.isValid()) {
         m_qml_target.write(m_default_value);
     } else {
         write_qml();
     }
-    m_complete = true;
+
+    const auto write_model_slot = this_meta->indexOfSlot("write_model()");
+
+    connect(m_qml_target.object(), m_qml_target.property().notifySignal(), this, this_meta->method(write_model_slot));
+    if (m_receive_model_updates) {
+        if (!m_meta_property.hasNotifySignal()) {
+            qWarning() << "Property " << m_property << " of object " << m_model << " has no notification signal!";
+            return;
+        }
+
+        const auto qml_write_slot = this_meta->indexOfSlot("write_qml()");
+        connect(m_model, m_meta_property.notifySignal(), this, this_meta->method(qml_write_slot));
+    }
 }
 
 void ModelBinding::setTarget(const QQmlProperty& property) { m_qml_target = property; }
@@ -86,9 +133,34 @@ void ModelBinding::set_property(const QString& new_property)
     emit property_changed(m_property);
 }
 
-void ModelBinding::write_qml() { m_qml_target.write(m_model_meta_property.read(m_model)); }
+void ModelBinding::write_qml()
+{
+    if (!m_complete)
+        return;
+    m_qml_target.write(read_model());
+}
 
-void ModelBinding::write_model() { m_model_meta_property.write(m_model, m_qml_target.read()); }
+void ModelBinding::write_model()
+{
+    if (!m_complete)
+        return;
+
+    if (m_gadget_meta_property.isValid()) {
+        auto gadget = m_gadget_meta_property.read(m_model);
+        m_meta_property.writeOnGadget(gadget.data(), m_qml_target.read());
+        m_gadget_meta_property.write(m_model, gadget);
+    }
+    m_meta_property.write(m_model, m_qml_target.read());
+}
+
+QVariant ModelBinding::read_model() const
+{
+    if (m_gadget_meta_property.isValid()) {
+        const auto gadget = m_gadget_meta_property.read(m_model);
+        return m_meta_property.readOnGadget(gadget.constData());
+    }
+    return m_meta_property.read(m_model);
+}
 
 const QVariant& ModelBinding::default_value() const { return m_default_value; }
 
--- app/ModelBinding.h
+++ app/ModelBinding.h
@@ -28,6 +28,7 @@ class ModelBinding : public QObject, public QQmlParserStatus, public QQmlPropert
     Q_PROPERTY(QObject* target READ model WRITE set_model NOTIFY model_changed FINAL)
     Q_PROPERTY(QString property READ property WRITE set_property NOTIFY property_changed FINAL)
     Q_PROPERTY(bool receive_model_updates READ receive_model_updates WRITE set_receive_model_updates NOTIFY receive_model_updates_changed FINAL)
+    Q_PROPERTY(QVariant default_value READ default_value WRITE set_default_value NOTIFY default_value_changed FINAL)
 public:
     explicit ModelBinding(QObject* parent = nullptr);
 
@@ -54,6 +55,9 @@ private slots:
     void write_qml();
     void write_model();
 
+private:
+    QVariant read_model() const;
+
 signals:
     void model_changed(QObject* model);
 
@@ -65,11 +69,11 @@ signals:
 
 private:
     QQmlProperty m_qml_target;
+    QMetaProperty m_gadget_meta_property;
+    QMetaProperty m_meta_property;
     QObject* m_model = nullptr;
     QString m_property;
-    QMetaProperty m_model_meta_property;
+    QVariant m_default_value;
     bool m_complete = false;
     bool m_receive_model_updates = false;
-    QVariant m_default_value;
-    Q_PROPERTY(QVariant default_value READ default_value WRITE set_default_value NOTIFY default_value_changed FINAL)
 };
