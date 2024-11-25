6723eaf4a8fff7a8ea6570e57e31b9d77b8e0fa1
Lucas Dworschak <e1225883@student.tuwien.ac.at>
2024-11-25 16:07:59 +0100

    Vector Layer - Rasterizer - moved tests to rasterizer.cpp - added sdf version - test sdf vs dda rasterizer

--- nucleus/utils/rasterizer.h
+++ nucleus/utils/rasterizer.h
@@ -16,11 +16,21 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *****************************************************************************/
 
+#include <array>
+#include <iostream>
 #include <vector>
 
 #include <glm/glm.hpp>
 
 namespace nucleus::utils::rasterizer {
+
+// Possible future improvements:
+// - DDA -> prevent fill_inner_triangle to be called twice for the same row (if we switch from one line to another)
+//      - also check overall how often each pixel is written to
+
+// TODOs:
+// - enlarge triangles
+// - line renderer
 
 namespace details {
 
@@ -33,51 +43,69 @@ namespace details {
 
     inline std::pair<glm::vec2, int> calculate_dda_steps(const glm::vec2 line)
     {
-        int steps = fabs(line.x) > fabs(line.y) ? fabs(line.x) : fabs(line.y);
+        int steps = ceil(fabs(line.x) > fabs(line.y) ? fabs(line.x) : fabs(line.y));
+        // should only be triggered if two vertices of a triangle are exactly on the same position
+        // -> the input data is wrong
+        assert(steps > 0);
+        // steps = std::max(steps, 1); // alternative force at least one dda step -> wrong output but it should at least "work"
 
         auto step_size = glm::vec2(line.x / float(steps), line.y / float(steps));
+
+        // double the steps -> otherwise we might miss important pixels
+        steps *= 2;
+        step_size /= 2.0f;
 
         return std::make_pair(step_size, steps);
     }
 
     template <typename PixelWriterFunction>
-    void dda_triangle_line(
-        const PixelWriterFunction& pixel_writer, const glm::vec2 origin0, const glm::vec2 line0, const glm::vec2 origin1, const glm::vec2 line1, unsigned int data_index, int fill_direction)
+    inline void fill_inner_triangle(
+        const PixelWriterFunction& pixel_writer, const glm::vec2& current_position, const glm::vec2& top_point, const glm::vec2& top_bottom_line, unsigned int data_index, int fill_direction)
     {
+        // calculate the x value of the top_bottom edge
+        int x = get_x_for_y_on_line(top_point, top_bottom_line, current_position.y) + fill_direction;
 
+        for (int j = current_position.x; j != x; j += fill_direction) {
+            pixel_writer(glm::vec2(j, current_position.y), data_index);
+        }
+    }
+
+    template <typename PixelWriterFunction>
+    void dda_triangle_line(const PixelWriterFunction& pixel_writer,
+        const glm::vec2& line_start_point,
+        const glm::vec2& line,
+        const glm::vec2& top_point,
+        const glm::vec2& top_bottom_line,
+        unsigned int data_index,
+        int fill_direction)
+    {
         glm::vec2 step_size;
         int steps;
-        std::tie(step_size, steps) = calculate_dda_steps(line0);
+        std::tie(step_size, steps) = calculate_dda_steps(line);
 
-        // make sure that at least one step is applied
-        steps = std::max(steps, 1);
+        int last_y = 0;
 
-        glm::vec2 current_start_position = origin0;
-
-        glm::vec2 current_position = current_start_position;
+        glm::vec2 current_position = line_start_point;
 
         for (int j = 0; j < steps; j++) {
             pixel_writer(current_position, data_index);
 
-            // add a write step to x/y -> this prevents holes from thickness steps to appear by making the lines thicker
-            if (step_size.x < 1)
-                pixel_writer(current_position + glm::vec2(1, 0) * glm::sign(step_size.x), data_index);
-            if (step_size.y < 1)
-                pixel_writer(current_position + glm::vec2(0, 1) * glm::sign(step_size.y), data_index);
-
             if (int(current_position.y + step_size.y) > current_position.y) { // next step would go to next y value
 
-                int x = get_x_for_y_on_line(origin1, line1, current_position.y);
-
-                for (int j = current_position.x; j != x; j += fill_direction) {
-                    pixel_writer(glm::vec2(j, current_position.y), data_index);
-                }
+                fill_inner_triangle(pixel_writer, current_position, top_point, top_bottom_line, data_index, fill_direction);
+                last_y = current_position.y;
             }
             current_position += step_size;
         }
+
+        current_position -= step_size;
+
+        // check if we have to apply the fill_inner_triangle alg for the current row
+        if (last_y != floor(current_position.y))
+            fill_inner_triangle(pixel_writer, current_position, top_point, top_bottom_line, data_index, fill_direction);
     }
 
-    template <typename PixelWriterFunction> void dda_triangle(const PixelWriterFunction& pixel_writer, std::vector<glm::vec2> triangle, unsigned int triangle_index)
+    template <typename PixelWriterFunction> void dda_triangle(const PixelWriterFunction& pixel_writer, const std::vector<glm::vec2>& triangle, unsigned int triangle_index)
     {
         auto edge_top_bottom = triangle[2] - triangle[0];
         auto edge_top_middle = triangle[1] - triangle[0];
@@ -113,15 +141,31 @@ namespace details {
         // // middle bottom line
         dda_triangle_line(pixel_writer, triangle[1], edge_middle_bottom, triangle[0], edge_top_bottom, triangle_index, fill_direction);
 
-        // DEBUG draw lines with certain index
-        // dda_line(triangle[0], edge_top_bottom, normal_top_bottom * 1.0f, 100, 0, true);
-        // dda_line(triangle[0], edge_top_middle, normal_top_middle * 1.0f, 100, fill_direction, true);
-        // dda_line(triangle[1], edge_middle_bottom, normal_middle_bottom * 1.0f, 100, fill_direction, true);
-
         // TODO enable again
         // add_end_cap(triangle_collection.cell_to_data, triangle[0], triangle_index, thickness);
         // add_end_cap(triangle_collection.cell_to_data, triangle[1], triangle_index, thickness);
         // add_end_cap(triangle_collection.cell_to_data, triangle[2], triangle_index, thickness);
+    }
+
+    template <typename PixelWriterFunction>
+    void triangle_sdf(const PixelWriterFunction& pixel_writer, const glm::vec2 current_position, const std::array<glm::vec2, 3> points, unsigned int data_index, float distance)
+    {
+        // https://iquilezles.org/articles/distfunctions2d/
+
+        glm::vec2 e0 = points[1] - points[0], e1 = points[2] - points[1], e2 = points[0] - points[2];
+        float s = glm::sign(e0.x * e2.y - e0.y * e2.x);
+
+        glm::vec2 v0 = current_position - points[0], v1 = current_position - points[1], v2 = current_position - points[2];
+        glm::vec2 pq0 = v0 - e0 * glm::clamp(glm::dot(v0, e0) / glm::dot(e0, e0), 0.0f, 1.0f);
+        glm::vec2 pq1 = v1 - e1 * glm::clamp(glm::dot(v1, e1) / glm::dot(e1, e1), 0.0f, 1.0f);
+        glm::vec2 pq2 = v2 - e2 * glm::clamp(glm::dot(v2, e2) / glm::dot(e2, e2), 0.0f, 1.0f);
+        glm::vec2 d
+            = min(min(glm::vec2(dot(pq0, pq0), s * (v0.x * e0.y - v0.y * e0.x)), glm::vec2(dot(pq1, pq1), s * (v1.x * e1.y - v1.y * e1.x))), glm::vec2(dot(pq2, pq2), s * (v2.x * e2.y - v2.y * e2.x)));
+        float dist_from_tri = -sqrt(d.x) * glm::sign(d.y);
+
+        if (dist_from_tri < distance) {
+            pixel_writer(current_position, data_index);
+        }
     }
 
 } // namespace details
@@ -130,7 +174,29 @@ namespace details {
 // output: top, middle, bottom, top, middle,...
 std::vector<glm::vec2> triangulize(std::vector<glm::vec2> polygons);
 
-template <typename PixelWriterFunction> void rasterize_triangle(const PixelWriterFunction& pixel_writer, std::vector<glm::vec2> triangles)
+// ideal if you want to rasterize only a few triangles, where every triangle covers a large part of the raster size
+// in this method every pixel traverses every triangle
+template <typename PixelWriterFunction> void rasterize_triangle_sdf(const PixelWriterFunction& pixel_writer, const std::vector<glm::vec2>& triangles, float distance, const glm::ivec2 grid_size)
+{
+    // we sample from the center
+    // and have a radius of a half pixel diagonal
+    // this still leads to false positives if the edge of a triangle is slighly in another pixel without every comming in this pixel
+    distance += sqrt(0.5);
+
+    for (uint8_t i = 0; i < grid_size.x; i++) {
+        for (uint8_t j = 0; j < grid_size.y; j++) {
+            auto current_position = glm::vec2 { i + 0.5, j + 0.5 };
+
+            for (size_t i = 0; i < triangles.size() / 3; ++i) {
+                details::triangle_sdf(pixel_writer, current_position, { triangles[i * 3 + 0], triangles[i * 3 + 1], triangles[i * 3 + 2] }, i, distance);
+            }
+        }
+    }
+}
+
+// ideal for many triangles. especially if each triangle only covers small parts of the raster
+// in this method every triangle is traversed only once, and it only accesses pixels it needs for itself.
+template <typename PixelWriterFunction> void rasterize_triangle(const PixelWriterFunction& pixel_writer, const std::vector<glm::vec2>& triangles)
 {
     for (size_t i = 0; i < triangles.size() / 3; ++i) {
         details::dda_triangle(pixel_writer, { triangles[i * 3 + 0], triangles[i * 3 + 1], triangles[i * 3 + 2] }, i);
---
+++ unittests/nucleus/rasterizer.cpp
@@ -0,0 +1,169 @@
+/*****************************************************************************
+ * AlpineMaps.org
+ * Copyright (C) 2024 Lucas Dworschak
+ * Copyright (C) 2024 Adam Celarek
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+
+#include <QSignalSpy>
+#include <catch2/catch_test_macros.hpp>
+
+#include <CDT.h>
+
+#include "nucleus/Raster.h"
+#include "nucleus/tile/conversion.h"
+#include "nucleus/utils/rasterizer.h"
+
+TEST_CASE("nucleus/rasterizer")
+{
+    SECTION("Triangulation")
+    {
+        // 5 point polygon
+        // basically a square where one side contains an inward facing triangle
+        // a triangulation algorithm should be able to discern that 3 triangles are needed to construct this shape
+        const std::vector<glm::vec2> points = { glm::vec2(0, 0), glm::vec2(1, 1), glm::vec2(0, 2), glm::vec2(2, 2), glm::vec2(2, 0) };
+        const std::vector<glm::ivec2> edges = { glm::ivec2(0, 1), glm::ivec2(1, 2), glm::ivec2(2, 3), glm::ivec2(3, 4), glm::ivec2(4, 0) };
+
+        CDT::Triangulation<double> cdt;
+        cdt.insertVertices(points.begin(), points.end(), [](const glm::vec2& p) { return p.x; }, [](const glm::vec2& p) { return p.y; });
+        cdt.insertEdges(edges.begin(), edges.end(), [](const glm::ivec2& p) { return p.x; }, [](const glm::ivec2& p) { return p.y; });
+        cdt.eraseOuterTrianglesAndHoles();
+
+        auto tri = cdt.triangles;
+        auto vert = cdt.vertices;
+
+        // check if only 3 triangles have been found
+        CHECK(tri.size() == 3);
+
+        // 1st triangle
+        CHECK(vert[tri[0].vertices[0]].x == 0.0);
+        CHECK(vert[tri[0].vertices[0]].y == 2.0);
+        CHECK(vert[tri[0].vertices[1]].x == 1.0);
+        CHECK(vert[tri[0].vertices[1]].y == 1.0);
+        CHECK(vert[tri[0].vertices[2]].x == 2.0);
+        CHECK(vert[tri[0].vertices[2]].y == 2.0);
+
+        // 2nd triangle
+        CHECK(vert[tri[1].vertices[0]].x == 1.0);
+        CHECK(vert[tri[1].vertices[0]].y == 1.0);
+        CHECK(vert[tri[1].vertices[1]].x == 2.0);
+        CHECK(vert[tri[1].vertices[1]].y == 0.0);
+        CHECK(vert[tri[1].vertices[2]].x == 2.0);
+        CHECK(vert[tri[1].vertices[2]].y == 2.0);
+
+        // 3rd triangle
+        CHECK(vert[tri[2].vertices[0]].x == 1.0);
+        CHECK(vert[tri[2].vertices[0]].y == 1.0);
+        CHECK(vert[tri[2].vertices[1]].x == 0.0);
+        CHECK(vert[tri[2].vertices[1]].y == 0.0);
+        CHECK(vert[tri[2].vertices[2]].x == 2.0);
+        CHECK(vert[tri[2].vertices[2]].y == 0.0);
+
+        // DEBUG print out all the points of the triangles (to check what might have went wrong)
+        // for (std::size_t i = 0; i < tri.size(); i++) {
+        //     printf("Triangle points: [[%f, %f], [%f, %f], [%f, %f]]\n",
+        //         vert[tri[i].vertices[0]].x, // x0
+        //         vert[tri[i].vertices[0]].y, // y0
+        //         vert[tri[i].vertices[1]].x, // x1
+        //         vert[tri[i].vertices[1]].y, // y1
+        //         vert[tri[i].vertices[2]].x, // x2
+        //         vert[tri[i].vertices[2]].y // y2
+        //     );
+        // }
+    }
+
+    SECTION("Triangle y ordering")
+    {
+        // make sure that the triangle_order function correctly orders the triangle points from lowest y to highest y value
+        const std::vector<glm::vec2> triangle_points_012 = { { glm::vec2(30, 10), glm::vec2(10, 30), glm::vec2(50, 50) } };
+        const std::vector<glm::vec2> triangle_points_021 = { glm::vec2(30, 10), glm::vec2(50, 50), glm::vec2(10, 30) };
+        const std::vector<glm::vec2> triangle_points_102 = { glm::vec2(10, 30), glm::vec2(30, 10), glm::vec2(50, 50) };
+        const std::vector<glm::vec2> triangle_points_201 = { glm::vec2(10, 30), glm::vec2(50, 50), glm::vec2(30, 10) };
+        const std::vector<glm::vec2> triangle_points_120 = { glm::vec2(50, 50), glm::vec2(30, 10), glm::vec2(10, 30) };
+        const std::vector<glm::vec2> triangle_points_210 = { glm::vec2(50, 50), glm::vec2(10, 30), glm::vec2(30, 10) };
+
+        const std::vector<glm::vec2> correct = { { glm::vec2(30, 10), glm::vec2(10, 30), glm::vec2(50, 50) } };
+
+        CHECK(correct == nucleus::utils::rasterizer::triangulize(triangle_points_012));
+        CHECK(correct == nucleus::utils::rasterizer::triangulize(triangle_points_021));
+        CHECK(correct == nucleus::utils::rasterizer::triangulize(triangle_points_102));
+        CHECK(correct == nucleus::utils::rasterizer::triangulize(triangle_points_201));
+        CHECK(correct == nucleus::utils::rasterizer::triangulize(triangle_points_120));
+        CHECK(correct == nucleus::utils::rasterizer::triangulize(triangle_points_210));
+    }
+
+    SECTION("rasterize triangle")
+    {
+        // two triangles
+        const std::vector<glm::vec2> triangles = { glm::vec2(30.5, 10.5), glm::vec2(10.5, 30.5), glm::vec2(50.5, 50.5), glm::vec2(5.5, 5.5), glm::vec2(15.5, 10.5), glm::vec2(5.5, 15.5) };
+
+        nucleus::Raster<uint8_t> output({ 64, 64 }, 0u);
+        const auto pixel_writer = [&output](glm::vec2 pos, int) { output.pixel(pos) = 255; };
+        nucleus::utils::rasterizer::rasterize_triangle(pixel_writer, triangles);
+
+        nucleus::Raster<uint8_t> output2({ 64, 64 }, 0u);
+        const auto pixel_writer2 = [&output2](glm::vec2 pos, int) { output2.pixel(pos) = 255; };
+        nucleus::utils::rasterizer::rasterize_triangle_sdf(pixel_writer2, triangles, 0, output2.size());
+
+        CHECK(output.buffer() == output2.buffer());
+
+        // DEBUG: save image (image saved to build/Desktop-Profile/unittests/nucleus)
+        // auto image = nucleus::tile::conversion::u8raster_to_qimage(output);
+        // image.save(QString("rasterizer_output.png"));
+        // auto image2 = nucleus::tile::conversion::u8raster_to_qimage(output2);
+        // image2.save(QString("rasterizer_output_sdf.png"));
+    }
+
+    SECTION("rasterize triangle small")
+    {
+        const std::vector<glm::vec2> triangles_small = { glm::vec2(2, 2), glm::vec2(1, 3), glm::vec2(3.8, 3.8) };
+        nucleus::Raster<uint8_t> output({ 6, 6 }, 0u);
+        const auto pixel_writer = [&output](glm::vec2 pos, int) { output.pixel(pos) = 255; };
+        nucleus::utils::rasterizer::rasterize_triangle(pixel_writer, triangles_small);
+
+        nucleus::Raster<uint8_t> output2({ 6, 6 }, 0u);
+        const auto pixel_writer2 = [&output2](glm::vec2 pos, int) { output2.pixel(pos) = 255; };
+        nucleus::utils::rasterizer::rasterize_triangle_sdf(pixel_writer2, triangles_small, 0, output2.size());
+
+        CHECK(output.buffer() == output2.buffer());
+
+        // DEBUG: save image (image saved to build/Desktop-Profile/unittests/nucleus)
+        // auto image = nucleus::tile::conversion::u8raster_to_qimage(output);
+        // image.save(QString("rasterizer_output_small.png"));
+        // auto image2 = nucleus::tile::conversion::u8raster_to_qimage(output2);
+        // image2.save(QString("rasterizer_output_small_sdf.png"));
+    }
+
+    SECTION("rasterize triangle smallest")
+    {
+        // less than one pixel
+        const std::vector<glm::vec2> triangles_smallest = { glm::vec2(30.4, 30.4), glm::vec2(30.8, 30.6), glm::vec2(30.8, 30.8) };
+        nucleus::Raster<uint8_t> output({ 64, 64 }, 0u);
+        const auto pixel_writer = [&output](glm::vec2 pos, int) { output.pixel(pos) = 255; };
+        nucleus::utils::rasterizer::rasterize_triangle(pixel_writer, triangles_smallest);
+
+        nucleus::Raster<uint8_t> output2({ 64, 64 }, 0u);
+        const auto pixel_writer2 = [&output2](glm::vec2 pos, int) { output2.pixel(pos) = 255; };
+        nucleus::utils::rasterizer::rasterize_triangle_sdf(pixel_writer2, triangles_smallest, 0, output2.size());
+
+        CHECK(output.buffer() == output2.buffer());
+
+        // DEBUG: save image (image saved to build/Desktop-Profile/unittests/nucleus)
+        // auto image = nucleus::tile::conversion::u8raster_to_qimage(output);
+        // image.save(QString("rasterizer_output_smallest.png"));
+        // auto image2 = nucleus::tile::conversion::u8raster_to_qimage(output2);
+        // image2.save(QString("rasterizer_output_smallest_sdf.png"));
+    }
+}
