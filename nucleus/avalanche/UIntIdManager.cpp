202c06ab68b877974925c146661f0b143ea973e0
joergchristian <e12213117@student.tuwien.ac.at>
2025-08-20 20:41:53 +0200

    eaws: load geojson from resource file

--- nucleus/avalanche/UIntIdManager.cpp
+++ nucleus/avalanche/UIntIdManager.cpp
@@ -51,8 +51,7 @@ UIntIdManager::UIntIdManager()
     for (int x = 2; x <= 8; x++) {
 
         // Read file
-        std::string applicationPath = QCoreApplication::applicationDirPath().toStdString();
-        QString filePath((applicationPath + std::string("/app/app/eaws/AT-0") + std::to_string(x) + std::string("_micro-regions.geojson.json")).c_str());
+        QString filePath((std::string(":/eaws/AT-0") + std::to_string(x) + std::string("_micro-regions.geojson.json")).c_str());
         QFile file(filePath);
         if (!file.open(QIODevice::ReadOnly)) {
             qWarning() << "Could not open file:" << filePath;
